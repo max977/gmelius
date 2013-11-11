7332990769afcc745ce8eb9fc961072336d13689
Florian Bersier <info@xpressyoo.com>
2013-11-11 01:58:45 +0100

    modified:   css/b.css 	modified:   help/index.html 	modified:   index.html 	modified:   js/a.js

--- js/a.js
+++ js/a.js
@@ -29,16 +29,17 @@ $(function() {
 		browser = 'Chrome';
 		config = 'To configure Gmelius in Chrome, go to the <a href="chrome-extension://dheionainndbbpoacpnopgmnihkcmnkl/options.html" target="_blank">options page</a> and select the <a href="../#features" title="view full list of features">features</a> you wish to activate. Don\'t forget to save your settings and check your Gmail inbox. <u>Tip</u>: To remove Gmelius icon from the Chrome toolbar, right-click on it and select "Hide Button".';
 		configbeta = 'To configure Gmelius in Chrome, go to the <a href="chrome-extension://kolcknldigimoipnaaalljghaahfpgbp/options.html" target="_blank">options page</a> and select the <a href="../#features" title="view full list of features">features</a> you wish to activate. Don\'t forget to save your settings and check your Gmail inbox.';
-		betalink = 'http://beta.gmelius.com/gmelius.crx';
+		betalink = '#';
 		compatibility = 'From Chrome 20';
 		compbeta = 'Compatibility | From Chrome 20';
 	} else if (isFirefox) {
 		browser = 'Firefox';
 		config = 'To configure Gmelius in Firefox, go to Tools <i class="icon-angle-right"></i> Add-ons <i class="icon-angle-right"></i> Extensions <i class="icon-angle-right"></i> Gmelius and click on "preferences". <a href="../#features" title="view full list of features">Features</a> are enabled by ticking their corresponding checkboxes. You will need to <a href="https://mail.google.com" title="reload Gmail now">reload Gmail</a> after changing your preferences.';
 		getlink = 'https://addons.mozilla.org/firefox/downloads/latest/120014/addon-120014-latest.xpi?src=external-gmelius';
-		betalink = 'http://beta.gmelius.com/gmelius.xpi';
+		betalink = '#';
 		compatibility = 'From Firefox 19';
 		compbeta = 'Compatibility | From Firefox 19';
+		
 	} else if (isOpera) {
 		browser = 'Opera';
 		config = 'To configure Gmelius in Opera, go to Extensions <i class="icon-angle-right"></i> Manage Extensions <i class="icon-angle-right"></i> Gmelius and click on the wrench icon <i class="icon-wrench"></i>. <a href="../#features" title="view full list of features">Features</a> are enabled by ticking their corresponding checkboxes. You may need to <a href="https://mail.google.com" title="reload Gmail now">reload Gmail</a> after changing your preferences.';
@@ -78,7 +79,7 @@ $(function() {
 	var share = '<ul><li class="go"><div class="g-plusone" data-size="medium" data-href="http://gmelius.com"></div><li class="fb"><div class="fb-like" data-href="https://www.facebook.com/adremover" data-send="false" data-layout="button_count" data-width="105" data-show-faces="false"></div><li class="tw"><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://gmelius.com">Tweet</a></ul>';
 	// Scrolling
 	$(".slow").on('click', function(event) {
-		// Identifier le lien cliqué dans la nav puis "capitalize" la 1ère lettre
+		// Identifier le lien cliqué dans la nav
 		var clicked = $(this).attr('href');
 		$('html, body').animate({
 			scrollTop: $(clicked).offset().top
@@ -114,6 +115,7 @@ $(function() {
 	$('#year').append(year);
 	$('#OS').append(OS);
 	$('#browser').append(browser);
+	$('#install-'+ browser).addClass('bold');
 	$('#brwver').append(browser + ' ' + version);
 	$('#share').append(share);
 	if ($('body').hasClass('hel')) {
@@ -130,11 +132,12 @@ $(function() {
 	if ($('.banner').hasClass('home')) {
 		$('#compatibility').append(compatibility);
 		if (isChrome) {
-			$("#download").click(function() {
+			$("#download,#install-Chrome").click(function() {
 				chrome.webstore.install()
 			});
 		} else {
 			$('#download').prop('href', getlink);
+			$('#install-Chrome').prop('href', 'https://chrome.google.com/webstore/detail/gmelius-ad-blocker-and-be/dheionainndbbpoacpnopgmnihkcmnkl');
 		}
 	} else if ($('.banner').hasClass('beta')) {
 		$('#download').prop('href', betalink);
