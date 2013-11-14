28414354901cd4fbc290fb0bac2eebace2b5429f
Florian Bersier <info@xpressyoo.com>
2013-11-14 17:30:07 +0100

    modified:   js/a.js

--- js/a.js
+++ js/a.js
@@ -98,20 +98,27 @@ $(function() {
 		compatibility = 'Browser not supported';
 	}
 	// Share SNS
-	(function(w, d, s) {
-  	function go(){
-   	 var js, fjs = d.getElementsByTagName(s)[0], load = function(url, id) {
-	  if (d.getElementById(id)) {return;}
-	  js = d.createElement(s); js.src = url; js.id = id;
-	  fjs.parentNode.insertBefore(js, fjs);
-	};
-		load('https://connect.facebook.net/en_US/all.js#xfbml=1&appId=234524816574248', 'facebook-jssdk');
-		load('https://apis.google.com/js/plusone.js');
-		load('https://platform.twitter.com/widgets.js', 'twitter-wjs');
-  }
-  if (w.addEventListener) { w.addEventListener("load", go, false); }
-  else if (w.attachEvent) { w.attachEvent("onload",go); }
-}(window, document, 'script'));
+	(function(doc, script) {
+ 	 var js, 
+      fjs = doc.getElementsByTagName(script)[0],
+      frag = doc.createDocumentFragment(),
+      add = function(url, id) {
+          if (doc.getElementById(id)) {return;}
+          js = doc.createElement(script);
+          js.src = url;
+          id && (js.id = id);
+          frag.appendChild( js );
+      };
+      
+    // Google+ button
+    add('https://apis.google.com/js/plusone.js');
+    // Facebook SDK
+    add('https://connect.facebook.net/en_US/all.js#xfbml=1&appId=234524816574248', 'facebook-jssdk');
+    // Twitter SDK
+    add('https://platform.twitter.com/widgets.js', 'twitter-wjs');
+
+    fjs.parentNode.insertBefore(frag, fjs);
+}(document, 'script'));
 	var share = '<ul><li class="go">' +
 				'<div class="g-plusone" data-size="medium" data-href="http://gmelius.com"></div>' +
 				'<li class="fb"><div class="fb-like" data-href="https://www.facebook.com/adremover" data-send="false"' +
